50451932349c1dcc16bb85d282b2e2028c6f704b
TarunikaSK <tarunikask1@gmail.com>
2026-05-30 22:38:08 +0530

    Removed bat file

--- stop.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-for /f "tokens=5" %%a in ('netstat -ano ^| findstr ":8000"') do taskkill /PID %%a /F
-for /f "tokens=5" %%a in ('netstat -ano ^| findstr ":8080"') do taskkill /PID %%a /F
-for /f "tokens=5" %%a in ('netstat -ano ^| findstr ":9000"') do taskkill /PID %%a /F
-
-echo All CASB services stopped.
